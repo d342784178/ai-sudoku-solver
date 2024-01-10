6ea82e975545e809242cf409a754e61fccfc91a6
zhichu <zhichu@qunhemail.com>
2024-01-10 12:50:20 +0800

    增加contentlayer支持

--- app/lib/util/commonUtil.ts
+++ app/lib/util/commonUtil.ts
@@ -1,4 +1,11 @@
 // 一个辅助函数生成类名字符串
+import clsx, {ClassValue} from "clsx";
+import {twMerge} from "tailwind-merge";
+
 export function classnames(...args: any[]) {
     return args.filter(Boolean).join(' ');
+}
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
 }
