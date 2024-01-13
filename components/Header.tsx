e25ab5a2cba22906249092efaccfd51375da4024
d342784178 <342784178@qq.com>
2024-01-13 16:05:00 +0800

    增加博文 优化排版 更新 sitemap

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ export function Header({}: {}) {
                      className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
                     <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                 </svg>
-                <h1 className="ml-3 text-xl">AI-Sudoku-Solver</h1>
+                <h1 className="ml-3 text-xl ">AI-Sudoku-Solver</h1>
             </Link>
 
             <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
@@ -19,7 +19,7 @@ export function Header({}: {}) {
                 <a href="https://github.com/d342784178/sudoku" target="_blank"
                    className="mr-5 hover:text-gray-900">
                     <svg stroke="currentColor" fill="currentColor" viewBox="0 0 16 16"
-                         className="text-xl" height="1em" width="1em"
+                         className="text-xl" height="27" width="27"
                          xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z">
@@ -28,7 +28,7 @@ export function Header({}: {}) {
                 </a>
                 <a href="https://twitter.com/eehaha46157" target="_blank" className="mr-5 hover:text-gray-900">
                     <svg stroke="currentColor" fill="currentColor" viewBox="0 0 16 16"
-                         className="text-xl" height="1em" width="1em"
+                         className="text-xl" height="27" width="27"
                          xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z">
