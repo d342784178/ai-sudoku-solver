8c6a205d1c297b7659740dfb9c269f8c40837064
d342784178 <342784178@qq.com>
2024-01-21 18:15:16 +0800

    修复表格没数据问题

--- components/step.tsx
+++ components/step.tsx
@@ -24,12 +24,12 @@ const columns: ColumnsType<UserStep> = [
         key: 'value',
         render: (value, record) => (<div>{record.value}</div>),
     },
-    {
-        title: 'Operate Time',
-        dataIndex: 'operate_time',
-        key: 'operate_time',
-        render: (value, record) => (<div>{record.create_time.toLocaleTimeString()}</div>),
-    },
+    // {
+    //     title: 'Operate Time',
+    //     dataIndex: 'operate_time',
+    //     key: 'operate_time',
+    //     render: (value, record) => (<div>{record.create_time.toLocaleTimeString()}</div>),
+    // },
 
 ];
 
@@ -39,11 +39,14 @@ export function Step({userSteps, onMouseEnterRecord, onMouseLeaveRecord}: {
     onMouseEnterRecord?: (record: UserStep, index: number) => void,
     onMouseLeaveRecord?: (record: UserStep, index: number) => void,
 }) {
+    const dataSource=userSteps&&userSteps.map((record, index)=>{
+        return {...record, key: index}
+    })
     return (
         <div className="flex flex-col items-center rounded-xl ">
             <h1 className="text-lg font-semibold ">User&apos;s Operation </h1>
             <div className="overflow-auto w-full">
-                <Table style={{width: '100%'}} columns={columns} dataSource={userSteps}
+                <Table style={{width: '100%'}} columns={columns} dataSource={dataSource}
                        pagination={{
                            pageSize: 5,
                            defaultCurrent: userSteps ? Math.floor(userSteps.length / 5) : 0,
