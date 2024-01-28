b6a6bb131c8e07001310189a207fa66d3756d4fd
d342784178 <342784178@qq.com>
2024-01-28 20:17:04 +0800

    支持保存用户 id

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,46 +1,44 @@
-import NextAuth from "next-auth"
+import NextAuth, {NextAuthOptions} from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import {HttpsProxyAgent} from "https-proxy-agent"
-import { PrismaClient } from "@prisma/client"
+import {PrismaAdapter} from "@auth/prisma-adapter"
+import {PrismaClient} from "@prisma/client"
 
 
 const prisma = new PrismaClient()
+export const authOptions: NextAuthOptions = {
+    // @ts-ignore
+    adapter: PrismaAdapter(prisma),
+    // 在 providers 中配置更多授权服务
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+            httpOptions: {
+                timeout: 10000,
+            },
+        }),
+        GoogleProvider({
+            clientId: '77759431340-cbpaoj16170l2gb45nco1o8r1qrbhmov.apps.googleusercontent.com',
+            clientSecret: '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',
+            httpOptions: {
+                timeout: 10000,
+            },
+        })
+        // ...add more providers here
+    ],
+    callbacks: {
+        session: async ({session, token, user}) => {
+            if (session?.user) {
+                session.user.id = user.id;
+            }
+            return session;
+        },
 
-const handler = NextAuth({
-        adapter: PrismaAdapter(prisma),
-        // 在 providers 中配置更多授权服务
-        providers: [
-            GithubProvider({
-                clientId: process.env.GITHUB_ID as string,
-                clientSecret: process.env.GITHUB_SECRET as string,
-                httpOptions: {
-                    timeout: 50000,
-                    agent: process.env.NEXT_AUTH_PROXY?new HttpsProxyAgent(process.env.NEXT_AUTH_PROXY):undefined
-                },
-            }),
-            GoogleProvider({
-                clientId: '77759431340-cbpaoj16170l2gb45nco1o8r1qrbhmov.apps.googleusercontent.com',
-                clientSecret: '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',
-                httpOptions: {
-                    timeout: 50000,
-                    agent: process.env.NEXT_AUTH_PROXY?new HttpsProxyAgent(process.env.NEXT_AUTH_PROXY):undefined
-                },
-            })
-            // ...add more providers here
-        ],
-        callbacks:{
-            session: async ({ session, token, user }) => {
-                      if (session?.user) {
-                        session.user.id = user.id;
-                      }
-                      return session;
-                    },
-                 
-        }
     }
-)
+}
+
+const handler = NextAuth(authOptions)
 
 
 export {handler as GET, handler as POST}
