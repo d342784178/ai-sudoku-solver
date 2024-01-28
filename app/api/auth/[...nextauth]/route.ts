dbbba2469293f4439fc8ed5a66fe7b13deaf72ce
d342784178 <d342784178@users.noreply.github.com>
2024-01-28 10:57:30 +0000

    github 登录本地集成

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,15 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import {HttpsProxyAgent} from "https-proxy-agent"
+import { PrismaClient } from "@prisma/client"
 
+
+const prisma = new PrismaClient()
 
 const handler = NextAuth({
+        adapter: PrismaAdapter(prisma),
         // 在 providers 中配置更多授权服务
         providers: [
             GithubProvider({
@@ -25,7 +30,17 @@ const handler = NextAuth({
             })
             // ...add more providers here
         ],
+        callbacks:{
+            session: async ({ session, token, user }) => {
+                      if (session?.user) {
+                        session.user.id = user.id;
+                      }
+                      return session;
+                    },
+                 
+        }
     }
 )
+
 
 export {handler as GET, handler as POST}
--- components/Login.tsx
+++ components/Login.tsx
@@ -4,15 +4,17 @@ import {signIn, signOut, useSession} from "next-auth/react"
 export default function Component() {
     const {data: session} = useSession()
     if (session && session.user) {
+        console.log("session", session)
         return (
             <>
-                <span className="mr-1">{session.user.email}</span>
-                <button onClick={() => signOut()}>登出</button>
+                <button className="mr-1" onClick={() => signOut()}>LogOut</button>
+                <img src={session.user.image||'/bgstar.svg'} alt="" className="round-full w-8"/>
+                {/* <span className="mr-1">{session.user.email}</span> */}
             </>
         )
     }
     return (
-        <button onClick={() => signIn()}>登录</button>
+        <button onClick={() => signIn()}>LogIn</button>
         // <button></button>
     )
 }
