7b669910ebb70827ed1314ee846a5ab9774984d4
zhichu <zhichu@qunhemail.com>
2024-02-29 13:58:37 +0800

    改gpt调用为服务端调用

--- app/api/ai/route.ts
+++ app/api/ai/route.ts
@@ -5,7 +5,6 @@ export async function POST(req: Request) {
     try {
         const reqJson = await req.json()
         const stream = await aiExplain(reqJson)
-        console.log(stream)
         return new Response(stream)
     } catch (error: any) {
         if (error.response) {
